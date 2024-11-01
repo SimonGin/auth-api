8790d5776a6ed0255c5a2c95c9a122713ec24c03
Simon Gin <tt26419@gmail.com>
2024-11-01 09:14:17 +0700

    Initalize project with Bun & Elysia

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .post("/user/register", () => {
+    return { success: true };
+  })
+  .post("/user/login", () => {
+    return { success: true };
+  })
+  .post("/user/logout", () => {
+    return { success: true };
+  });
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.listen(3000);
+
+console.log(`Elys is running at ${app.server?.hostname}:${app.server?.port}`);
