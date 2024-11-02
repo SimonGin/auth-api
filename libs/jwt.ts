211a2ec0fc000ecf71699f90892df17be6cfd487
Simon Gin <tt26419@gmail.com>
2024-11-02 15:20:37 +0700

    Get user's profile

--- libs/jwt.ts
+++ libs/jwt.ts
@@ -1,6 +1,11 @@
 import { jwt } from "@elysiajs/jwt";
 
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET is not defined in the environment variables.");
+}
+
 export const jwtConfig = jwt({
   name: "jwt_auth",
-  secret: "thekeytousemyjwtforsignandverify",
+  secret: JWT_SECRET,
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Elysia } from "elysia";
 import { jwtConfig } from "@/libs/jwt";
+import prismadb from "@/libs/prismadb";
 import authRoutes from "./routes/auth";
 import cors from "@elysiajs/cors";
+
+const port = process.env.PORT || 1863;
 
 const app = new Elysia()
   .use(cors())
@@ -24,10 +27,31 @@ const app = new Elysia()
         }
       },
     },
-    (app) => app.get("/", () => "Hello Elysia")
+    (app) =>
+      app.get("/me", async ({ auth_user }) => {
+        if (auth_user) {
+          const uid = auth_user.uid.toString();
+          const foundUser = await prismadb.user.findUnique({
+            where: { id: uid },
+            select: {
+              id: true,
+              name: true,
+              email: true,
+              createdAt: true,
+            },
+          });
+          return {
+            status: 200,
+            msg: "Success Retrieving User's Profile",
+            metadata: foundUser,
+          };
+        } else {
+          return { status: 500, msg: "Internal Server Error" };
+        }
+      })
   )
   .use(authRoutes);
 
-app.listen(1863);
+app.listen(port);
 
 console.log(`Running at ${app.server?.hostname}:${app.server?.port}`);
